eeb938f7cc25e2402c889b67f53ef1d156b37869
Oleg Salionov <oleg@salionov.ru>
2014-12-04 19:10:59 +1000

    fix for ejabberd 2.1.x

--- src/mod_zeropush.erl
+++ src/mod_zeropush.erl
@@ -31,7 +31,6 @@
 -behaviour(gen_mod).
 
 -export([start/2,
-	 init/2,
 	 stop/1,
 	 send_notice/3]).
 
@@ -40,19 +39,15 @@
 -include("ejabberd.hrl").
 -include("jlib.hrl").
 
-start(Host, Opts) ->
-    ?INFO_MSG("Starting mod_zeropush", [] ),
-    register(?PROCNAME,spawn(?MODULE, init, [Host, Opts])),  
-    ok.
-
-init(Host, _Opts) ->
+start(Host, _Opts) ->
+    ?INFO_MSG("Starting mod_zeropush for host \"~s\"", [Host] ),
     inets:start(),
     ssl:start(),
     ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, send_notice, 10),
     ok.
 
 stop(Host) ->
-    ?INFO_MSG("Stopping mod_zeropush", [] ),
+    ?INFO_MSG("Stopping mod_zeropush for host \"~s\"", [Host] ),
     ejabberd_hooks:delete(offline_message_hook, Host,
 			  ?MODULE, send_notice, 10),
     ok.
